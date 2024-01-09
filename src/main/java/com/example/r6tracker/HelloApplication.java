21e96928448bc8b361963a36372bcccf4c873217
Pruts-Hacker <gj-reijnders@home.nl>
2024-01-09 19:50:14 +0100

    added new label and textfields to the first screen wich is going to be the log in screen.

--- src/main/java/com/example/r6tracker/HelloApplication.java
+++ src/main/java/com/example/r6tracker/HelloApplication.java
@@ -2,7 +2,13 @@ package com.example.r6tracker;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +16,24 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+
+        Label txtWelkom = new Label("Welkom bij de R6TRACKER");
+
+        VBox inlog = new VBox();
+        TextField txtNaam = new TextField();
+        txtNaam.setPromptText("Gebruikersnaam");
+        TextField txtWachtwoord = new TextField();
+        txtWachtwoord.setPromptText("Wachtwoord");
+
+        inlog.getChildren().addAll(txtNaam, txtWachtwoord);
+        root.add(txtWelkom,1,0);
+        root.add(inlog,1,1);
+
+        Scene scene = new Scene(root, 1200, 600);
+        stage.setTitle("R6TRACKER!");
         stage.setScene(scene);
         stage.show();
     }
